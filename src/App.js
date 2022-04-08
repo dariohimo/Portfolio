b9822af3d409974b8966a309af72db67fd0f75c8
dariohimo <dariohimo@gmail.com>
2022-04-08 18:44:27 -0500

    change url

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
-import Work from './components/work'
+
 import './App.scss'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+        <Route path="/" element={<Layout />} >
+          <Route path='/' element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/work" element={<Work />} />
+          
         </Route>
       </Routes>
     </>
